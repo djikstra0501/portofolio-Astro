080920b1b1414b23922f1fb5326fad07bfd08f62
djikstra0501 <dipastra04@gmail.com>
2026-07-20 12:35:38 +0700

    Mode Change to Static

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,6 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
-import node from '@astrojs/node';
-
-// https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: node({
-    mode: 'standalone'
-  })
+  // no `output:` (defaults to 'static'), no `adapter:`
+  site: 'https://portofolio.dananjaya.my.id',
 });
